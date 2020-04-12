e3d25fd44573055a5aa713ceddf3e8a321d5b67a
Sebastian Nilsson <sebbehebbe@gmail.com>
2020-04-12 11:17:38 +0200

    Initial commit

---
+++ bot_v_bot.py
@@ -0,0 +1,26 @@
+from dlgo.agent import naive
+from dlgo import goboard_slow
+from dlgo import gotypes
+from dlgo.utils import print_board, print_move
+import time
+import random
+
+
+def main():
+    board_size = 9
+    game = goboard_slow.GameState.new_game(board_size)
+    bots = {
+        gotypes.Player.black: naive.RandomBot(),
+        gotypes.Player.white: naive.RandomBot(),
+    }
+    while not game.is_over():
+
+        print(chr(27) + "[2j")
+        print_board(game.board)
+        bot_move = bots[game.next_player].select_move(game)
+        print_move(game.next_player, bot_move)
+        game = game.apply_move(bot_move)
+
+
+if __name__ == "__main__":
+    main()
---
+++ game_tests.py
@@ -0,0 +1,32 @@
+import unittest
+from dlgo.gotypes import *
+from dlgo.goboard_slow import *
+
+
+class TestGame(unittest.TestCase):
+
+    """ If a stone is placed, the board should return 
+    that stone when queried """
+
+    def setUp(self):
+        self.board = Board(19, 19)
+        self.player = Player.black
+        self.board = Board(19, 19)
+
+    def test_place_stone(self):
+        move = Point(5, 5)
+        self.board.place_stone(self.player, move)
+        new_stone = self.board.get(Point(5, 5))
+        self.assertEqual(new_stone, Player.black)
+
+    """ It should not be possible to place stones outside 
+    the grid """
+
+    def test_place_stone_outside_grid(self):
+        move = Point(20, 20)
+        with self.assertRaises(AssertionError):
+            self.board.place_stone(self.player, move)
+
+
+if __name__ == '__main__':
+    unittest.main()
